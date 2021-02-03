911e4dabd24035f2e188f8bb6b6bb73a02e4caee
Florian-Rowehy <florian.rowehy@gmail.com>
2021-02-03 11:56:35 +0100

    added basic router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import Header from './Component/Header'
 import Navbar from './Component/Navbar';
 import HomePage from './Page/HomePage';
+import  {BrowserRouter , Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Navbar></Navbar>
-      <div className='container pt-5'>
-        <HomePage/>
-      </div>
+    <BrowserRouter >
+      <Navbar/>
+      <main className='container pt-5'>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+        </Switch>
+      </main>
       <Header/>
-    </>
+    </BrowserRouter >
   );
 }
 
