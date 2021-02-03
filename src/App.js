5554d1932b98fc083b19f492e4bb941a80f8510f
Florian-Rowehy <florian.rowehy@gmail.com>
2021-02-03 18:37:36 +0100

    changed App to stateful component, added axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import './App.css';
-import Header from './Component/Header'
+import React from 'react';
+import axios from 'axios';
 import Navbar from './Component/Navbar';
 import HomePage from './Page/HomePage';
 import  {BrowserRouter , Switch, Route} from 'react-router-dom';
+import ItemsPage from './Page/ItemsPage';
 
-function App() {
-  return (
-    <BrowserRouter >
-      <Navbar/>
-      <main className='container pt-5'>
-        <Switch>
-          <Route exact path='/' component={HomePage}/>
-        </Switch>
-      </main>
-      <Header/>
-    </BrowserRouter >
-  );
+class App extends React.Component {
+  state = {
+    categories: [],
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000/api/categories')
+            .then(res => res.data['hydra:member'])
+            .then(
+              data => {
+                console.log(data);
+                this.setState({categories: data})
+              }
+              )
+            .catch(e=>{console.log(e);})
+  }
+
+  render() {
+    return (
+      <BrowserRouter >
+        <Navbar/>
+        <main>
+          <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route exact path='/items' component={ItemsPage}/>
+          </Switch>
+        </main>
+      </BrowserRouter >
+    );
+  }
 }
 
 export default App;
