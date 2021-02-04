cce6cce253a6067357546e01fbd31efa1e746799
Florian-Rowehy <florian.rowehy@gmail.com>
2021-02-04 18:46:57 +0100

    added link to Article on ArticleCard

--- src/Component/ArticleCard.js
+++ src/Component/ArticleCard.js
@@ -1,14 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const ArticleCard = (props) => {
-    const style = {width: '18rem'}
     return (
-        <div className="card" style={style}>
+        <div className="card col-4 col-md-3 px-0">
             <img className="card-img-top" src={props.image} alt=""/>
             <div className="card-body">
-                <h5 className="card-title h6">{props.name}</h5>
-                {/* <p className="card-text">{props.description}</p> */}
-                <a href="/" className="btn btn-primary">Go somewhere</a>
+                <NavLink to={'/article/'+ props.id } className="card-title h6">{props.name}</NavLink>
             </div>
         </div>
     );
