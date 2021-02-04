927c7f0e2314840d107a23167397414192104c0f
Florian-Rowehy <florian.rowehy@gmail.com>
2021-02-04 12:49:37 +0100

    changed a to NavLink in navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import authAPI from "../Service/authAPI";
 
 const Navbar = (props) => {
@@ -7,7 +8,7 @@ const Navbar = (props) => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">TheBalooCompany</a>
+      <NavLink className="navbar-brand" to="/">TheBalooCompany</NavLink>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -15,30 +16,20 @@ const Navbar = (props) => {
       <div className="collapse navbar-collapse" id="navbarColor01">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/women">Women</a>
+            <NavLink className="nav-link" to="/women">Women</NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/men">Men</a>
+            <NavLink className="nav-link" to="/men">Men</NavLink>
           </li>
           <li className="nav-ite">
-            <a className="nav-link" href="/kids">Kids</a>
+            <NavLink className="nav-link" to="/kids">Kids</NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/login">Login</a>
+            <NavLink className="nav-link" to="/login">Login</NavLink>
           </li>
           <li className="nav-item">
             <button className="nav-link btn btn-danger" onClick={logoutClickHandler}>Logout</button>
           </li>
-          {/* <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div className="dropdown-menu">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <a className="dropdown-item" href="#">Something else here</a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">Separated link</a>
-            </div>
-          </li> */}
         </ul>
         <form className="form-inline my-2 my-lg-0">
           <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
