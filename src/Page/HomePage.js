9e27d074625dea06d63365290ade9765b82fa604
Florian-Rowehy <florian.rowehy@gmail.com>
2021-02-04 09:00:06 +0100

    updated HomePage, has sidebar & articledisplay

--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import ArticleDisplay from '../Component/ArticleDisplay';
+import SideBar from '../Component/SideBar';
 
 const HomePage = (props) => {
     return (
-        <div class="jumbotron">
-            <h1 class="display-3">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4"/>
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <p class="lead">
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            </p>
+        <div className="d-flex">
+            <SideBar categories={props.categories} classifications={props.classifications}/>
+            <ArticleDisplay/>
         </div>
     )
 }
