a4ae71ca10fbe23aeec4519ffd9aecda248f58e0
Florian-Rowehy <florian.rowehy@gmail.com>
2021-02-04 15:15:48 +0100

    removed uncessary dependencies

--- src/Component/ArticleDisplay.js
+++ src/Component/ArticleDisplay.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import { useLocation } from 'react-router-dom'
-import axios from 'axios';
 import ArticleCard from './ArticleCard';
 import Pagination from './Pagination';
 import appService from '../Service/appService';
