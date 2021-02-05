e9ddb87a155aa49ac53a0dc08e9813e161487961
Florian-Rowehy <florian.rowehy@gmail.com>
2021-02-05 06:38:32 +0100

    changed display, addCartButton functional in Article

--- src/Component/Article.js
+++ src/Component/Article.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect}  from "react";
 import { useParams } from 'react-router-dom'
 import articlesAPi from '../Service/articlesAPI';
+import usersAPI from "../Service/usersAPI";
+
 
 const Article = (props) => {
     const [article, setArticle] = useState(null);
@@ -10,11 +12,20 @@ const Article = (props) => {
         articlesAPi.findArticle(id)
              .then(data => {
                 setArticle(data);
-                console.log(data);
                 })
              .catch(e => {console.log(e);})
     }, [id]);
     
+    const addToCart = () => {
+        try {
+            usersAPI
+                .addToCart(id)
+                .then(()=>{props.history.replace('/cart');});
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
     const style = {
         color: '#5f8b2f',
         fontSize: '1.5rem',
@@ -26,10 +37,15 @@ const Article = (props) => {
             <div className='container'>
                 <h2 className='text-center'>{article.name}</h2>
                 <img className='mx-auto mb-2' src={article.image} alt=""/>
-                <p className='my-0'>Brand: {article.brand.name}</p>
-                <p className='my-0'>Category: {article.categories.map((category, i)=><span key={i}>{category.name}</span>)}</p>
-                <p className='mt-0'>Classification: {article.articleClassification.name}</p>
-                <p>Price: {article.price}€ <i className="fas fa-cart-arrow-down" style={style}></i></p>
+                <p className='my-0'>Brand: {capitalize(article.brand.name)}</p>
+                <p className='my-0'>Category: {article.categories.map((category, i)=><span key={i}>{capitalize(category.name)}</span>)}</p>
+                <p className='mt-0'>Classification: {capitalize(article.articleClassification.name)}</p>
+                <p>
+                    Price: {article.price}€ 
+                    <button className="border-0 bg-transparent" type="button" onClick={addToCart}>
+                        <i className="fas fa-cart-arrow-down" style={style}></i>
+                    </button>
+                </p>
                 <div>
                     <p className='mb-0'>Description:</p>
                     <p>{article.description}</p>
@@ -43,5 +59,10 @@ const Article = (props) => {
     }
     
 }
+
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
 
 export default Article;
