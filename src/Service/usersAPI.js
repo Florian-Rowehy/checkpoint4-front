ded9b45509111b018d347f2bc90e6ca8df49d9ae
Florian-Rowehy <florian.rowehy@gmail.com>
2021-02-05 06:36:35 +0100

    created function getCart & addToCart in usersAPI

--- src/Service/usersAPI.js
+++ src/Service/usersAPI.js
@@ -1,9 +1,28 @@
 import appService from './appService';
+import axios from 'axios';
+
+const url = "http://localhost:8000/api";
 
 function findAll() {
     return appService.find('/users');
 }
 
-const usersAPI
+function addToCart(articleId) {
+    return axios
+        .post(url+"/articles/"+articleId+"/add", {})
+        .then(res => res.data)
+}
+
+function getCart() {
+    return axios
+        .get(url+'/cart')
+        .then(res => res.data);
+}
+
+const usersAPI = {
+    findAll,
+    addToCart,
+    getCart
+}
 
 export default usersAPI;
